4a047db822cf7f6f1737ca939001f489e1b5d253
Lawrence-Tang <tangzongsheng@gmail.com>
2024-05-28 11:04:14 +0800

    Initial support for rk3528

---
+++ rk3528/base.mk
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TARGET_PLAT=rk3528
+TARGET_ARCH=arm64
+CROSS_COMPILER=aarch64-linux-gnu-
+TARGET_OSNAME=friendlywrt
+
+# FriendlyWRT
+# 
+
+TARGET_FRIENDLYWRT_CONFIG=rockchip
+FRIENDLYWRT_SRC=friendlywrt
+FRIENDLYWRT_PATCHS+=("feeds/luci/;device/common/src-patchs/23.05/feeds/luci")
+FRIENDLYWRT_FILES+=(device/common/ntfs3)
+FRIENDLYWRT_FILES+=(device/common/uas)
+FRIENDLYWRT_FILES+=(device/common/nft-fullcone)
+FRIENDLYWRT_FILES+=(device/common/emmc-tools)
+FRIENDLYWRT_FILES+=(device/common/distfeeds)
+FRIENDLYWRT_FILES+=(device/common/default-settings)
+
+# U-boot
+# 
+
+TARGET_UBOOT_CONFIG=nanopi_zero2_defconfig
+
+# Kernel
+# 
+
+TARGET_KERNEL_CONFIG="nanopi5_linux_defconfig friendlywrt.config kvm.config"
+TARGET_KERNEL_DTB=resource.img
+
+# Misc
+# 
+FRIENDLYWRT_ROOTFS=build_dir/target-aarch64_generic_musl/root-rockchip
+FRIENDLYWRT_PACKAGE_DIR=bin/targets/rockchip/armv8/packages
+TARGET_IMAGE_DIRNAME=friendlywrt23
+TARGET_SD_RAW_FILENAME=friendlywrt_23.05_$(date +%Y%m%d)_rk3528_arm64_sd.img
+TARGET_EFLASHER_RAW_FILENAME=friendlywrt_23.05_$(date +%Y%m%d)_rk3528_arm64_eflasher.img
